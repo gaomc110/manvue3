4980ab3f51908c51832494ccacf18a4961419cb1
gaomc110 <984239684@qq.com>
2023-03-02 18:15:31 +0800

    Delete dist directory

--- dist/config.js
+++
@@ -1,5 +0,0 @@
-window.PAGECONF = {
-  hostApi: 'https://testonline-platformapi.vesync.com',
-  hostLogin: 'http://52.82.122.82/#/',  // https://auth.itekcity.com/#/
-  appKey: '246b2841db3089760ccdb4b5d75bc7c8d75bc7c8'
-}
